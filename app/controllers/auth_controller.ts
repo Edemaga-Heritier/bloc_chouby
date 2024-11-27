52da5463e94bde921504de9d7011d0df49768f75
Edemaga-Heritier <hedemaga@gmail.com>
2024-11-27 11:33:21 +0100

    ajouter une route handleRegister

--- app/controllers/auth_controller.ts
+++ app/controllers/auth_controller.ts
@@ -4,6 +4,10 @@ export default class AuthController {
   register({ view }: HttpContext) {
     return view.render('pages/auth/register')
   }
+
+  async handleRegister({ request }: HttpContext) {
+    return request.all()
+  }
   login({ view }: HttpContext) {
     return view.render('pages/auth/login')
   }
